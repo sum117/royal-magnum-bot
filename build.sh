5e3c32d3abc9a761933675b688785ffe64121914
sum117 <jvcaliman@hotmail.com>
2024-01-02 01:17:05 -0300

    ci: Add build script and update package.json

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+src_dir="src/assets"
+build_dir="build/assets"
+
+if [ -d "$src_dir" ]; then
+    mkdir -p "$build_dir"
+
+    cp "$src_dir"/* "$build_dir"
+
+    echo "Assets moved successfully."
+else
+    echo "Source directory '$src_dir' not found."
+fi
